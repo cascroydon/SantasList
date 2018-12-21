6a59ae013cd67cbc11e4db34b84212f0c95af8d8
Paul Sinnett <paul.sinnett@gmail.com>
2018-12-21 09:06:28 +0000

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+	struct NaughtyChild
+	{
+		public int ChildID;
+		public string Name;
+		public int Age;
+		public bool isAlwaysNaughty;
+	}
+
+	public static void Main(string[] args)
+	{
+		var santasList = new Dictionary<string, NaughtyChild>();
+		NaughtyChild Matthew;
+		NaughtyChild Oliver;
+
+		Matthew.ChildID = 1;
+		Matthew.Name = "Matthew";
+		Matthew.Age = 16;
+		Matthew.isAlwaysNaughty = true;
+
+		Oliver.ChildID = 2;
+		Oliver.Name = "Oliver";
+		Oliver.Age = 17;
+		Oliver.isAlwaysNaughty = true;
+
+		santasList.Add("Matthew", Matthew);
+		santasList.Add("Oliver", Oliver);
+
+		Console.WriteLine("Please enter student");
+		string SelectedChild = Console.ReadLine();
+
+		Console.WriteLine("ID is                : {0}", santasList[SelectedChild].ChildID);
+		Console.WriteLine("Full name is         : {0}", santasList[SelectedChild].Name);
+		Console.WriteLine("Age is               : {0}", santasList[SelectedChild].Age);
+		Console.WriteLine("On the naughty list? : {0}", santasList[SelectedChild].isAlwaysNaughty);
+	}
+}
